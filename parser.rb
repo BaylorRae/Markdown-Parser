05513ff9ae1f7f79f0f21b518931f583fadfa85a
Baylor Rae <baylorrae@gmail.com>
2011-12-28 15:16:01 -0600

    live version

--- parser.rb
+++ parser.rb
@@ -3,10 +3,7 @@ require "sinatra"
 require "redcarpet"
 
 get "/" do
-  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
-    :autolink => true, :space_after_headers => true)
-    
-  markdown.render("## This is an H2 Tag!\nAwesome huh?")
+  "I'm alive, I'm alive, I'm ... why are you not excited?"
 end
 
 post "/parse" do
